7eeda46b67fa72fcd897027027a6adbccb5c113d
RamsesCode <RamsesSanchez25@gmail.com>
2025-06-20 15:47:18 -0400

    comments section to keep track of assignment

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+/*
+[✅] Add rows to the grid
+[✅] Add columns to the grid
+[ ] Remove rows from the grid
+[ ] Remove columns from the grid
+[ ] Select a color from a dropdown menu of colors
+[ ] Click on a single cell, changing its color to the currently selected color
+[ ] Fill all uncolored cells with the currently selected color
+[ ] Fill all cells with the currently selected color
+[ ] Clear all cells / restore all cells to their original/initial color
+[ ] Click and hold (mouseover) from a single cell (start) to a different cell (end) such that all affected/hovered-over cells from start to end change to the currently selected color
+*/
+
+
+
 const tbody = document.querySelector("tbody");
 const addRowBtn = document.getElementById("add-row");
 const addColumnBtn = document.getElementById("add-column");
